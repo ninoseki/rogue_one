7bfd16c876dc6e2df9f970e08e0f00628a41d781
Manabu Niseki <manabu.niseki@gmail.com>
2019-04-30 20:01:14 +0900

    fix: fix opposite specs

--- spec/detector_spec.rb
+++ spec/detector_spec.rb
@@ -1,7 +1,22 @@
 # frozen_string_literal: true
 
-RSpec.describe RogueOne do
-  it "has a version number" do
-    expect(RogueOne::VERSION).not_to be nil
+RSpec.describe RogueOne::Detector do
+  subject { described_class.new(target: "1.1.1.1") }
+
+  describe "#report" do
+    before do
+      allow(subject).to receive(:top_100_domains).and_return(%w(google.com))
+      allow(Parallel).to receive(:processor_count).and_return(0)
+    end
+
+    let(:report) { subject.report }
+
+    it do
+      expect(report.dig(:verdict)).to eq("benign one")
+    end
+
+    it do
+      expect(report.dig(:landing_pages)).to eq([])
+    end
   end
 end
